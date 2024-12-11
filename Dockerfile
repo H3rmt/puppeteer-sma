9ae97dac7d7257e354347203ff5af3ef2deba151
Enrico Stemmer <enrico@h3rmt.zip>
2024-12-11 02:37:14 +0100

    initial Commit

---
+++ Dockerfile
@@ -0,0 +1,49 @@
+FROM node:slim
+RUN apt-get update && apt-get install -y \
+    libasound2 \
+    libatk-bridge2.0-0 \
+    libatk1.0-0 \
+    libatspi2.0-0 \
+    libc6 \
+    libcairo2 \
+    libcups2 \
+    libdbus-1-3 \
+    libdrm2 \
+    libexpat1 \
+    libgbm1 \
+    libglib2.0-0 \
+    libnspr4 \
+    libnss3 \
+    libpango-1.0-0 \
+    libpangocairo-1.0-0 \
+    libstdc++6 \
+    libudev1 \
+    libuuid1 \
+    libx11-6 \
+    libx11-xcb1 \
+    libxcb-dri3-0 \
+    libxcb1 \
+    libxcomposite1 \
+    libxcursor1 \
+    libxdamage1 \
+    libxext6 \
+    libxfixes3 \
+    libxi6 \
+    libxkbcommon0 \
+    libxrandr2 \
+    libxrender1 \
+    libxshmfence1 \
+    libxss1 \
+    libxtst6 \
+    && rm -rf /var/lib/apt/lists/*
+
+RUN groupadd -r pptruser && useradd -r -g pptruser -G audio,video pptruser \
+    && mkdir -p /home/pptruser/Downloads \
+    && chown -R pptruser:pptruser /home/pptruser
+USER pptruser
+
+WORKDIR /app
+COPY package.json pnpm-lock.yaml ./
+RUN corepack pnpm install
+COPY index.js ./
+CMD ["node", "index.js"]
---
+++ index.js
@@ -0,0 +1,52 @@
+import puppeteer from 'puppeteer';
+
+const browser = await puppeteer.launch({
+    browser: "chrome",
+    args: ['--no-sandbox'],
+});
+const page = await browser.newPage();
+try {
+    while (true) {
+        console.log("Navigating to login page")
+        await page.goto('https://ennexos.sunnyportal.com/login', {waitUntil: 'networkidle0'});
+        await page.setViewport({width: 1920, height: 1080});
+        if (await page.$("#onetrust-accept-btn-handler") !== null) {
+            console.log("Accepting cookies")
+            await page.click("#onetrust-accept-btn-handler");
+        }
+        // wait a bit to close popup (else login button doesn't work)
+        await new Promise(resolve => setTimeout(resolve, 500))
+        await page.click('#login > button');
+        console.log("Login button clicked")
+        await page.waitForSelector('#username');
+        await page.type('#username', process.env.SUNNY_USERNAME);
+        await page.type('#password', process.env.SUNNY_PASSWORD);
+        await page.keyboard.press('Enter');
+        console.log("Login form submitted")
+        await new Promise(resolve => setTimeout(resolve, 3000)) // wait for login to succeed
+        await page.goto('https://ennexos.sunnyportal.com/14654281,14193940/dashboard', {waitUntil: 'networkidle0'});
+        console.log("Navigated to dashboard")
+        await new Promise(resolve => setTimeout(resolve, 1000))
+
+        while (true) {
+            await page.screenshot({
+                path: 'images/screenshot.png'
+            });
+            // console.log("Screenshot taken")
+
+            // check if logged out
+            console.log("url", page.url())
+            if (!page.url().includes('14654281,14193940/dashboard')) {
+                break;
+            }
+            await new Promise(resolve => setTimeout(resolve, 10000))
+        }
+    }
+} catch (e) {
+    console.log(e)
+    await page.screenshot({
+        path: 'images/screenshot-exit.png'
+    });
+} finally {
+    await browser.close();
+}
